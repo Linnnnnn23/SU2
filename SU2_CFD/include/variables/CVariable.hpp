cbbd783f9ab8b6bdbddd562c0cdd162fafd677c1
Linnnnnn23 <135637515+Linnnnnn23@users.noreply.github.com>
2025-06-03 20:17:19 +0800

    Update CVariable.hpp

--- SU2_CFD/include/variables/CVariable.hpp
+++ SU2_CFD/include/variables/CVariable.hpp
@@ -395,7 +395,7 @@ public:
    */
   inline virtual void SetDES_LengthScale(unsigned long iPoint, su2double val_des_lengthscale) {}
 
-  inline virtual void SetDebug_Quantities(CConfig, config, unsigned long iPoint, su2double val_VTM_Average, su2double val_FKH_Average) {}
+  inline virtual void SetDebug_Quantities(CConfig *config, unsigned long iPoint, su2double val_VTM_Average, su2double val_FKH_Average) {}
     
   inline virtual su2double GetVTM_Average(unsigned long iPoint) const { return 0.0;}
   inline virtual su2double GetFKH_Average(unsigned long iPoint) const { return 0.0;}
